407411dc54b7a032be1333339c1c6c6eac788a35
thethirdlane <anil@panix.com>
2018-04-04 16:02:55 +0530

    commit

--- src/ttl/app/RegApp.java
+++ src/ttl/app/RegApp.java
@@ -14,63 +14,72 @@ public class RegApp {
 		RegApp ra = new RegApp();
 		ra.go();
 	}
-	
+
 	public void go() {
-		StudentService service = new StudentService(); 
+		StudentService service = new StudentService();
 
 		List<Student> students = service.getAllStudents();
-		
+
 		Collections.sort(students);
-		
-		for(Student s : students) {
+
+		for (Student s : students) {
 			System.out.println(s);
 		}
 
 		NameComparator nc = new NameComparator();
 		Collections.sort(students, nc);
-		
+
+		Collections.sort(students, new Comparator<Student>() {
+			@Override
+			public int compare(Student o1, Student o2) {
+				 return o1.getName().compareTo(o2.getName());
+			};
+		});
+
+		Collections.sort(students, (o1, o2) -> o1.getName().compareTo(o2.getName()));
+
+		students.forEach((s) -> System.out.println(s));
+
+		students.forEach(System.out::println);
+		students.forEach(RegApp::myHandler);
+
 		System.out.println();
-		for(Student s : students) {
+		for (Student s : students) {
 			System.out.println(s);
 		}
-		
+
 	}
+
 	
-	
+	public static void myHandler(Student s) {
+		//Send down socket
+	}
+
 	class NameComparator implements Comparator<Student> {
 
 		@Override
 		public int compare(Student o1, Student o2) {
-			int r =  o1.getName().compareTo(o2.getName());
-			if(r == 0) {
+			int r = o1.getName().compareTo(o2.getName());
+			if (r == 0) {
 				r = o1.getId() - o2.getId();
 			}
 			return r;
 		}
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	public static void mainEnum(String[] args) {
-		
-		StudentService service = new StudentService(); 
-		
+
+		StudentService service = new StudentService();
+
 		String name = "Joe";
 		String strStatus = "Part_time";
-		
+
 		Student s = new Student(name, Status.fromPrettyString(strStatus));
-		
+
 		System.out.println(s);
-		
-		Status [] statuses = Status.values();
-		for(Status st : statuses) {
+
+		Status[] statuses = Status.values();
+		for (Status st : statuses) {
 			System.out.println("code is " + st.getCode());
 		}
 	}
