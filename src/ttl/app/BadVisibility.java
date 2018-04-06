6a7b15c8dd02659209de046dcc13a5533ce19e27
thethirdlane <anil@panix.com>
2018-04-06 14:36:46 +0530

    commit

---
+++ src/ttl/app/BadVisibility.java
@@ -0,0 +1,40 @@
+package ttl.app;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class BadVisibility {
+
+	private volatile boolean ready;
+	private int var;
+	
+	public static void main(String [] args) {
+		
+		BadVisibility bv = new BadVisibility();
+		bv.start();
+		
+	}
+	
+	public void start() {
+		Thread th1 = new Worker1();
+		Thread th2 = new Worker2();
+		
+		th2.start();
+		th1.start();
+	}
+	
+	public class Worker1 extends Thread
+	{
+		public void run() {
+			while(!ready);
+			System.out.println("Var is " + var);
+		}
+	}
+	
+	public class Worker2 extends Thread
+	{
+		public void run() {
+			var = 10;
+			ready = true;
+		}
+	}
+}
